99d59b56083e9a54fa6311b5def4ceaab939727c
Mostafa Shaker <iampopmostafa@gmail.com>
2026-02-19 01:49:42 +0200

    Added Makefile

--- build.sh
+++
@@ -1,5 +0,0 @@
-nasm -f bin boot.asm -o bin/boot.bin
-$HOME/opt/cross/bin/$TARGET-gcc -ffreestanding -m32 -g -c kernel.c -o bin/kernel.o
-nasm -f elf kernel_entry.asm -o bin/kernel_entry.o
-$HOME/opt/cross/bin/$TARGET-ld -o bin/full_kernel.bin -Ttext 0x1000 bin/kernel_entry.o bin/kernel.o --oformat binary
-cat bin/boot.bin bin/full_kernel.bin >bin/everything.bin
