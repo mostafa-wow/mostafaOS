b9bee7968ad183f877d4c1e25409350e8d8772da
Mostafa Shaker <iampopmostafa@gmail.com>
2026-02-24 01:23:32 +0200

    Improved itoa

--- src/kernel.c
+++ src/kernel.c
@@ -12,6 +12,35 @@ static inline void outb(uint16_t port, uint8_t val) {
   __asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
 }
 
+char *itoa(int value, char *result, int base) {
+  // check that the base if valid
+  if (base < 2 || base > 36) {
+    *result = '\0';
+    return result;
+  }
+
+  char *ptr = result, *ptr1 = result, tmp_char;
+  int tmp_value;
+
+  do {
+    tmp_value = value;
+    value /= base;
+    *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrst"
+             "uvwxyz"[35 + (tmp_value - value * base)];
+  } while (value);
+
+  // Apply negative sign
+  if (tmp_value < 0)
+    *ptr++ = '-';
+  *ptr-- = '\0';
+  while (ptr1 < ptr) {
+    tmp_char = *ptr;
+    *ptr-- = *ptr1;
+    *ptr1++ = tmp_char;
+  }
+  return result;
+}
+
 void update_cursor(int x, int y) {
   uint16_t pos = y * VGA_WIDTH + x;
 
@@ -19,48 +48,6 @@ void update_cursor(int x, int y) {
   outb(0x3D5, (uint8_t)(pos & 0xFF));
   outb(0x3D4, 0x0E);
   outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
-}
-
-void itoa(int32_t value, char *buffer) {
-  char temp[12];
-  int i = 0;
-  int is_negative = 0;
-
-  if (value == 0) {
-    buffer[0] = '0';
-    buffer[1] = '\0';
-    return;
-  }
-
-  if (value < 0) {
-    is_negative = 1;
-    value = -value;
-  }
-
-  while (value > 0) {
-    temp[i++] = '0' + (value % 10);
-    value /= 10;
-  }
-
-  if (is_negative)
-    temp[i++] = '-';
-
-  // Reverse string into buffer
-  int j;
-  for (j = 0; j < i; j++)
-    buffer[j] = temp[i - j - 1];
-
-  buffer[i] = '\0';
-}
-
-void htoa(uint32_t value, char *buffer, int uppercase) {
-  const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
-  int i;
-  for (i = 0; i < 8; i++) {
-    uint8_t nibble = (value >> ((7 - i) * 4)) & 0xF;
-    buffer[i] = digits[nibble];
-  }
-  buffer[8] = '\0';
 }
 
 void kputc(uint8_t c, uint8_t colour, uint8_t x, uint8_t y) {
@@ -92,9 +79,18 @@ void kernel_main() {
   kprint("This is my new self, Renewed. Recharged. Refocused. "
          "With nothing to stop me from being me.\n",
          0xF4);
-  char number[32];
-  itoa(676767, number);
+
   kprint("value:", 0x0F);
-  kprint(number, 0x0F);
+  char num1[32];
+  itoa(67, num1, 10);
+  kprint(num1, 0x0F);
+
+  kprint("\n", 0x0);
+
+  kprint("value:", 0x0F);
+  char num2[32];
+  itoa(0x7c00, num2, 16);
+  kprint(num2, 0x0F);
+
   return;
 }
