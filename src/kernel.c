5af2b1e27027717c1968989c56e08427e49b3374
Mostafa Shaker <iampopmostafa@gmail.com>
2026-02-23 19:39:29 +0200

    Added iota and htoa

--- src/kernel.c
+++ src/kernel.c
@@ -21,6 +21,48 @@ void update_cursor(int x, int y) {
   outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
 }
 
+void itoa(int32_t value, char *buffer) {
+  char temp[12];
+  int i = 0;
+  int is_negative = 0;
+
+  if (value == 0) {
+    buffer[0] = '0';
+    buffer[1] = '\0';
+    return;
+  }
+
+  if (value < 0) {
+    is_negative = 1;
+    value = -value;
+  }
+
+  while (value > 0) {
+    temp[i++] = '0' + (value % 10);
+    value /= 10;
+  }
+
+  if (is_negative)
+    temp[i++] = '-';
+
+  // Reverse string into buffer
+  int j;
+  for (j = 0; j < i; j++)
+    buffer[j] = temp[i - j - 1];
+
+  buffer[i] = '\0';
+}
+
+void htoa(uint32_t value, char *buffer, int uppercase) {
+  const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+  int i;
+  for (i = 0; i < 8; i++) {
+    uint8_t nibble = (value >> ((7 - i) * 4)) & 0xF;
+    buffer[i] = digits[nibble];
+  }
+  buffer[8] = '\0';
+}
+
 void kputc(uint8_t c, uint8_t colour, uint8_t x, uint8_t y) {
   if (c == '\n') {
     current_active_column++;
@@ -48,7 +90,11 @@ void kprint(uint8_t *s, uint8_t colour) {
 void kernel_main() {
   kprint("mostafaOS\n", 0x0F);
   kprint("This is my new self, Renewed. Recharged. Refocused. "
-         "With nothing to stop me from being me.",
+         "With nothing to stop me from being me.\n",
          0xF4);
+  char number[32];
+  itoa(676767, number);
+  kprint("value:", 0x0F);
+  kprint(number, 0x0F);
   return;
 }
