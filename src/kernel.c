3307bbe917ffd3a279729f181be90638f87e09fb
Mostafa Shaker <iampopmostafa@gmail.com>
2026-02-25 01:14:40 +0200

    Added polling based serial port driver

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 
+#define PORT 0x3f8
+
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
@@ -10,6 +12,12 @@ uint8_t current_active_column = 0;
 
 static inline void outb(uint16_t port, uint8_t val) {
   __asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline uint8_t inb(uint16_t port) {
+  uint8_t ret;
+  __asm__ volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+  return ret;
 }
 
 char *itoa(int value, char *result, int base) {
@@ -74,6 +82,55 @@ void kprint(uint8_t *s, uint8_t colour) {
   update_cursor(current_active_row, current_active_column);
 }
 
+int8_t serial_init() {
+  outb(PORT + 1, 0x00); // Disable all interrupts
+  outb(PORT + 3, 0x80); // Enable DLAB (set baud rate divisor)
+  outb(PORT + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+  outb(PORT + 1, 0x00); //                  (hi byte)
+  outb(PORT + 3, 0x03); // 8 bits, no parity, one stop bit
+  outb(PORT + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+  outb(PORT + 4, 0x0B); // IRQs enabled, RTS/DSR set
+  outb(PORT + 4, 0x1E); // Set in loopback mode, test the serial chip
+  outb(PORT + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial
+                        // returns same byte)
+
+  // Check if serial is faulty (i.e: not same byte as sent)
+  if (inb(PORT + 0) != 0xAE) {
+    return 1;
+  }
+
+  // If serial is not faulty set it in normal operation mode
+  // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+  outb(PORT + 4, 0x0F);
+  return 0;
+}
+
+int32_t serial_received() { return inb(PORT + 5) & 1; }
+
+uint8_t read_serial() {
+  while (serial_received() == 0)
+    ;
+
+  return inb(PORT);
+}
+
+int32_t is_transmit_empty() { return inb(PORT + 5) & 0x20; }
+
+void write_serial(char a) {
+  while (is_transmit_empty() == 0)
+    ;
+
+  outb(PORT, a);
+}
+
+void serial_print(char *s) {
+  int32_t i = 0;
+  while (s[i] != 0) {
+    write_serial(s[i]);
+    i++;
+  }
+}
+
 void kernel_main() {
   kprint("mostafaOS\n", 0x0F);
   kprint("This is my new self, Renewed. Recharged. Refocused. "
@@ -91,6 +148,9 @@ void kernel_main() {
   char num2[32];
   itoa(0x7c00, num2, 16);
   kprint(num2, 0x0F);
+
+  serial_init();
+  serial_print("mostafaOS\n");
 
   return;
 }
